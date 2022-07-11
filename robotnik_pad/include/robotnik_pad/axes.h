279932799122301e7b54fc27f17d638a9b6c09ef
Luis <luisebt995@gmail.com>
2022-07-11 13:14:38 +0200

    Changes to manage Axes with class vector.

--- robotnik_pad/include/robotnik_pad/axes.h
+++ robotnik_pad/include/robotnik_pad/axes.h
@@ -1,47 +1,88 @@
-#ifndef _BUTTON_
-#define _BUTTON_
+#ifndef _AXES_
+#define _AXES_
 
 //! Class to save the state of the buttons
-class Button
+class Axes
 {
-  int is_pressed_;
-  bool is_released_;
+  int is_pressed_pos, is_pressed_neg;
+  bool is_released_pos, is_released_neg;
+	float axis_value;
 
 public:
-  Button()
+  Axes()
   {
-    is_pressed_ = 0;
-    is_released_ = false;
+    is_pressed_pos = 0;
+		is_pressed_neg = 0;
+    is_released_pos = false;
+		is_released_neg = false;
+		axis_value = 0.0;
   }
 
   //! Set the button as 'pressed'/'released'
-  void press(int value)
+  void press(float value)
   {
-    if (is_pressed_ and !value)
+    if (is_pressed_pos and !(value > 0.01))
     {
-      is_released_ = true;
+      is_released_pos = true;
     }
-    else if (is_released_ and value)
-      is_released_ = false;
+    else if (is_released_pos and (value > 0.01))
+			is_released_pos = false;
 
-    is_pressed_ = value;
+		if (is_pressed_neg and !(value < 0.01))
+		{
+			is_released_neg = true;
+		}
+		else if (is_released_neg and (value < 0.01))
+			is_released_neg = false;
+
+		if (value > 0.01)
+			is_pressed_pos = true;
+		else
+			is_pressed_pos = false;
+
+		if (value < 0.01)
+			is_pressed_neg = true;
+		else
+			is_pressed_neg = false;
+
+		axis_value = value;
   }
 
-  int isPressed() const
+  int isPressedPos() const
   {
-    return is_pressed_;
+    return is_pressed_pos;
+  }
+	int isPressedNeg() const
+  {
+    return is_pressed_neg;
   }
 
-  bool isReleased() const
+  bool isReleasedPos() const
   {
-    return is_released_;
+    return is_released_pos;
   }
 
-  void resetReleased()
+	bool isReleasedNeg() const
   {
-    if (is_released_)
-      is_released_ = false;
+    return is_released_neg;
   }
+
+  void resetReleasedPos()
+  {
+    if (is_released_pos)
+      is_released_pos = false;
+  }
+
+	void resetReleasedNeg()
+  {
+    if (is_released_neg)
+      is_released_neg = false;
+  }
+
+	float getValue() const
+	{
+		return axis_value;
+	}
 };
 
-#endif  // _BUTTON_
+#endif  // _AXES_
